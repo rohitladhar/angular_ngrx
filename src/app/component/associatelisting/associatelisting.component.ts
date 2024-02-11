27ed68b968216f231f10c8209b0056d7f1976e26
Rohini Ladhar <rohit.ladhar10@gmail.com>
2024-02-11 13:19:28 +0000

    initial commit

---
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-associatelisting',
+  standalone: true,
+  imports: [],
+  templateUrl: './associatelisting.component.html',
+  styleUrl: './associatelisting.component.css'
+})
+export class AssociatelistingComponent {
+
+}
